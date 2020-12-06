425c43eb2a108e45f53b4a4e90ea2d3c13947851
Shpakouski <job.shpakouski@gmail.com>
2020-12-06 11:39:44 +0300

    solution #2 some implementations

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const util = require('util');
 
+// Method #2
+// const lstat = util.promisify(fs.lstat);
 
-fs.readdir(process.cwd(), (err, filenames) => {
+// Method #3
+// const lstat = fs.promises.lstat;
+const {
+    lstat
+} = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
     if (err) {
         throw new Error(err);
     }
 
-    const allStats = Array(filenames.length).fill(null);
-
     for (let filename of filenames) {
-        const index = filenames.indexOf(filename);
+        try {
+            const stats = await lstat(filename);
 
-        fs.lstat(filename, (err, stats) => {
-            if (err) {
-                throw new Error(err);
-            }
-
-            allStats[index] = stats;
-
-            const ready = allStats.every(stats => {
-                return stats;
-            });
-
-            if (ready) {
-                allStats.forEach((stats, index) => {
-                    console.log(filenames[index], stats.isFile());
-                });
-            }
-        });
+            console.log(filename, stats.isFile());
+        } catch (err) {
+            throw new Error(err);
+        }
     }
 });
+
+// Method #1
+// const lstat = filename => {
+//     return new Promise((resolve, reject) => {
+//         fs.lstat(filename, (err, stats) => {
+//             if (err) {
+//                 reject(err);
+//             }
+//             resolve(stats);
+//         })
+//     });
+// }

193fbd1111c6f3258529cb9121956511c0432f58
Shpakouski <job.shpakouski@gmail.com>
2020-12-07 12:24:16 +0300

    accepting command line arguments

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const util = require('util');
 const chalk = require('chalk');
+const path = require('path');
+
+const targetDir = process.argv[2] || process.cwd();
 
 const {
     lstat
 } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+fs.readdir(targetDir, async (err, filenames) => {
     if (err) {
         throw new Error(err);
     }
 
     const statPromises = filenames.map(filename => {
-        return lstat(filename);
+        return lstat(path.join(targetDir, filename));
     });
 
     const allStats = await Promise.all(statPromises);
@@ -23,9 +25,9 @@ fs.readdir(process.cwd(), async (err, filenames) => {
         const index = allStats.indexOf(stats);
 
         if (stats.isFile()) {
-            console.log(chalk.black.bgWhite(filenames[index]));
+            console.log(chalk.magenta(filenames[index]));
         } else {
-            console.log(chalk.bgGreen(filenames[index]));
+            console.log(chalk.cyan(filenames[index]));
         }
     }
 });

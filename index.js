d51bc3400e0aaceec69c6a06a4d80e1ad638f11d
Shpakouski <job.shpakouski@gmail.com>
2020-12-07 10:39:02 +0300

    console log with colors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const util = require('util');
+const chalk = require('chalk');
 
 const {
     lstat
@@ -21,6 +22,10 @@ fs.readdir(process.cwd(), async (err, filenames) => {
     for (let stats of allStats) {
         const index = allStats.indexOf(stats);
 
-        console.log(filenames[index], stats.isFile());
+        if (stats.isFile()) {
+            console.log(chalk.black.bgWhite(filenames[index]));
+        } else {
+            console.log(chalk.bgGreen(filenames[index]));
+        }
     }
 });
